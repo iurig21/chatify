aa1569f8c1cfdeaa874916858bb5e0fdb37e16b2
iurig21 <a32192@alunos.ipca.pt>
2025-10-11 20:14:00 +0100

    feat: login and logout endpoints implemented

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { signup } from '../controllers/authControllers.js';
+import { login, signup,logout } from '../controllers/authControllers.js';
 
 const router = express.Router();
 
 router.post("/signup",signup)
+router.post("/login",login)
+router.post("/logout",logout)
 
 export default router
