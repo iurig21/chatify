1231211b0556449f316d9a1dc3391e24a8194a3f
iurig21 <a32192@alunos.ipca.pt>
2025-10-19 20:54:23 +0100

    fix: ui fixes

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -16,8 +16,8 @@ function SignUpPage() {
 
   return (
     <div className="w-full flex items-center justify-center p-4 bg-slate-900">
-      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px]">
-        <BorderAnimatedContainer>
+      <div className="relative w-full max-w-5xl md:h-[675px] h-[650px]">
+        <BorderAnimatedContainer> 
           <div className="w-full flex flex-col md:flex-row">
             <div className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30">
               <div className="w-full max-w-md">
