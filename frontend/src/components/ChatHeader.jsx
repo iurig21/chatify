3b97d97d8de34d9aac1b628f098505b359c335a1
iurig21 <a32192@alunos.ipca.pt>
2025-10-21 00:18:50 +0100

    fix: fix esc event listner

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -3,34 +3,41 @@ import { useChatStore } from "../store/useChatStore.js";
 import { useEffect } from "react";
 
 function ChatHeader() {
+  const { selectedUser, setSelectedUser } = useChatStore();
 
-const {selectedUser,setSelectedUser} = useChatStore()
+  useEffect(() => {
+    const handleEscClick = (e) => {
+      if (e.key === "Escape") setSelectedUser(null);
+    };
 
-useEffect(() => {
-    window.addEventListener("keydown", (event) => {
-       if(event.key === "Escape") setSelectedUser(null);
-       window.removeEventListener("keydown")
-    })
-},[setSelectedUser])
+    window.addEventListener("keydown", handleEscClick);
+    return () => window.removeEventListener("keydown", handleEscClick);
+  }, [setSelectedUser]);
 
   return (
     <div className="flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 h-[4rem] max-h-[84px] px-6 flex-1">
-        <div className="flex items-center space-x-3">
-            <div className="avatar avatar-online">
-                <div className="w-12 rounded-full">
-                    <img src={selectedUser.profilePic || "/avatar.png"} alt={selectedUser.fullName} />
-                </div>
-            </div>
-            <div>
-                <h3 className="text-slate-200 font-medium"> {selectedUser.fullName} </h3>
-                <p className="text-slate-400 text-sm">Online</p>
-            </div>
+      <div className="flex items-center space-x-3">
+        <div className="avatar avatar-online">
+          <div className="w-12 rounded-full">
+            <img
+              src={selectedUser.profilePic || "/avatar.png"}
+              alt={selectedUser.fullName}
+            />
+          </div>
         </div>
-        <button onClick={() => setSelectedUser(null)}>
-            <XIcon className="w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer"/>
-        </button>
-    </div>  
-  )
+        <div>
+          <h3 className="text-slate-200 font-medium">
+            {" "}
+            {selectedUser.fullName}{" "}
+          </h3>
+          <p className="text-slate-400 text-sm">Online</p>
+        </div>
+      </div>
+      <button onClick={() => setSelectedUser(null)}>
+        <XIcon className="w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer" />
+      </button>
+    </div>
+  );
 }
 
-export default ChatHeader
+export default ChatHeader;
