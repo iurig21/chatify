fa88e6e5e4fa7a78f113cc41f358a6a42cde1d28
iurig21 <a32192@alunos.ipca.pt>
2025-10-23 14:05:34 +0100

    fix: fix import

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,7 +2,6 @@ import { create } from "zustand";
 import { api } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
-import { disconnect } from "mongoose";
 
 const BASE_URL =
   import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
